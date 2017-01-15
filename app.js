23321d3c7578359531cdaaaf0be3587870813456
utleroy <bryanhankins@me.com>
2017-01-15 13:12:41 -0600

    add multiple dice

--- app.js
+++ app.js
@@ -2,40 +2,30 @@
 	const app = angular.module('fetchApp', []);
 
 	app.controller('MainController', function($scope){
-		$scope.diceRoll = {
-			diceNum: [1,2,3,4,5,6]
-			
-		};
-		
 		$scope.playerScore = [];
+		$scope.hand = function(){
+			$scope.diceNum = Math.floor(Math.random() * 5) + 1;
+			if($scope.diceNum == 1){
+				$scope.showDice1 = true;
+			}else if($scope.diceNum == 2){
+				$scope.showDice2 = true;
+			}else if($scope.diceNum == 3){
+				$scope.showDice3 = true;
+			}else if($scope.diceNum == 4){
+				$scope.showDice4 = true;
+			}else if($scope.diceNum == 5){
+				$scope.showDice5 = true;
+			}
+			console.log("Dice Roll ", $scope.diceNum);
+		}	
+			$scope.hand();
+		
+			$scope.Dice2 = $scope.hand();
+			$scope.Dice3 = $scope.hand();
+			$scope.Dice4 = $scope.hand();
+			$scope.Dice5 = $scope.hand();
 
-		$scope.diceNum = Math.floor(Math.random() * 5) + 1;
-		if($scope.diceNum == 1){
-			$scope.showDice1 = true;
-		}else if($scope.diceNum == 2){
-			$scope.showDice2 = true;
-		}else if($scope.diceNum == 3){
-			$scope.showDice3 = true;
-		}else if($scope.diceNum == 4){
-			$scope.showDice4 = true;
-		}else if($scope.diceNum == 5){
-			$scope.showDice5 = true;
-		}
-
-		$scope.diceNum2 = Math.floor(Math.random() * 5) + 1;
-		if($scope.diceNum2 == 1){
-			$scope.showDice1 = true;
-		}else if($scope.diceNum2 == 2){
-			$scope.showDice2 = true;
-		}else if($scope.diceNum2 == 3){
-			$scope.showDice3 = true;
-		}else if($scope.diceNum2 == 4){
-			$scope.showDice4 = true;
-		}else if($scope.diceNum2 == 5){
-			$scope.showDice5 = true;
-		}
-		console.log("Player 1: ", $scope.diceNum);
-		$scope.playerScore = $scope.diceNum + $scope.diceNum2;
+		
 
 	});
 })();
